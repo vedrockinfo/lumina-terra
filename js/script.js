a0b1226ebdc8585e318054d27099253a649c908a
VedantCMS <vedantgupta.cms@gmail.com>
2025-02-26 14:13:19 +0530

    second

--- js/script.js
+++ js/script.js
@@ -5,3 +5,66 @@ jQuery(window).scroll(function () {
         jQuery("header").removeClass("fixed-header")
     }
 })
+
+$(document).ready(function () {
+    $("#collapsable").click(function () {
+        $(".nav-list").toggleClass("active"); // Toggle 'active' class
+    });
+});
+
+
+$(document).ready(function() {
+
+    $('.tab_header .item').on("click",function(){
+        var number = $(this).data("option");
+    
+        $('.tab_header .item').removeClass('active');
+    
+        $(this).addClass('active');
+    
+        $('.tab_container .container_item').hide();
+        $('div[data-item="'+ number +'"]').show();
+    });
+    
+    });
+    $('.curriculum-slider').owlCarousel({
+                loop: true,
+                margin: 50,
+                nav: false,
+                dots: false,
+                navText: ['<i class="fa-solid fa-arrow-left"></i>', '<i class="fa-solid fa-arrow-right"></i>'],
+                autoplay: false,
+                autoplayTimeout: 7000,
+                smartSpeed: 800,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 2
+                    },
+                    1000: {
+                        items: 2,
+                        stagePadding: 220,
+                    }
+                }
+            })
+    
+            $(document).ready(function () {
+      // Open modal
+      $('#foundation').on('click', function () {
+        $('#myModal').fadeIn();
+      });
+    
+      // Close modal
+      $('.close').on('click', function () {
+        $('#myModal').fadeOut();
+      });
+    
+      // Close modal when clicking outside the content
+      $(window).on('click', function (e) {
+        if ($(e.target).is('#myModal')) {
+          $('#myModal').fadeOut();
+        }
+      });
+    });
